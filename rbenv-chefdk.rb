2cbf66d414946a87631f4e00bc06978b860c6f3f
Ryan McKern <ryan@orangefort.com>
2017-05-13 03:43:00 -0700

    Fix missing dependency & caveats in rbenv-chefdk
    Forgot to require rbenv and tell users how to complete installation.

--- rbenv-chefdk.rb
+++ rbenv-chefdk.rb
@@ -5,8 +5,26 @@ class RbenvChefdk < Formula
 
   bottle :unneeded
 
+  depends_on :rbenv
+
   def install
     prefix.install Dir["*"]
+  end
+
+  def caveats; <<-EOS.undent
+    This formula does not depend on or install ChefDK for you. There's
+    too many different ways to ensure that it's installed so that was
+    left as an exercise for the user.
+
+    To complete installation, run:
+      $ mkdir "$(rbenv root)/versions/chefdk"
+
+    You can confirm that this is set up correctly
+      $ rbenv shell chefdk
+      $ rbenv rehash
+      $ rbenv which ruby
+      /opt/chefdk/embedded/bin/ruby
+    EOS
   end
 
   test do
